896503a5f3ba4cb0b8e9ecbd68b10682ee9ca45e
AckimJnr <acklongwe@gmail.com>
2023-11-16 08:30:34 +0200

    Initial project setup

---
+++ api/database/migrations/2023_11_15_213044_create_user_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->integer('user_detail_id', true);
+            $table->string('account_id')->index('account_id');
+            $table->string('district_code')->index('district_code');
+            $table->string('physical_address');
+            $table->string('country');
+            $table->string('phone_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
