6d8c7d3b732c2e37034d206e00a3e330004a1039
AckimJnr <acklongwe@gmail.com>
2023-11-20 15:43:33 +0200

    Add database dump

--- nss_database_dump.sql
+++ nss_database_dump.sql
@@ -64,8 +64,41 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'2023_11_15_213044_create_districts_table',0),(2,'2023_11_15_213044_create_roles_table',0),(3,'2023_11_15_213044_create_user_details_table',0),(4,'2023_11_15_213044_create_users_table',0),(5,'2023_11_15_213047_add_foreign_keys_to_roles_table',0),(6,'2023_11_15_213047_add_foreign_keys_to_user_details_table',0),(7,'2023_11_16_212621_create_districts_table',1),(8,'2023_11_16_212621_create_roles_table',1),(9,'2023_11_16_212621_create_user_details_table',1),(10,'2023_11_16_212621_create_users_table',1),(11,'2023_11_16_212624_add_foreign_keys_to_roles_table',1),(12,'2023_11_16_212624_add_foreign_keys_to_user_details_table',1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+DROP TABLE IF EXISTS `personal_access_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=121 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal_access_tokens`
+--
+
+LOCK TABLES `personal_access_tokens` WRITE;
+/*!40000 ALTER TABLE `personal_access_tokens` DISABLE KEYS */;
+INSERT INTO `personal_access_tokens` VALUES (108,'App\\Models\\User','nss655b259740528','auth_token','df82b0ef4c73bcedcbbdda3a79cb2c140e2ab0911bbb7ed9a8e448315205f5cb','[\"*\"]',NULL,NULL,'2023-11-20 07:23:51','2023-11-20 07:23:51'),(109,'App\\Models\\User','nss655b259740528','auth_token','d6183750cebe45dc2f562ea939f6ce2b3b14e94c94bd1703c34b83a6e32a53a2','[\"*\"]',NULL,NULL,'2023-11-20 07:24:02','2023-11-20 07:24:02'),(110,'App\\Models\\User','nss655b259740528','auth_token','99dd355fbe4fb9be3564ce9137230b72715a5e64517d530d16c8f6855ee6fe16','[\"*\"]',NULL,NULL,'2023-11-20 07:25:45','2023-11-20 07:25:45'),(111,'App\\Models\\User','nss655b259740528','auth_token','4867e3ba7bc4843107ed70649332bd85fa329ecdcf0a6ac02839fec13697b3f2','[\"*\"]',NULL,NULL,'2023-11-20 07:25:51','2023-11-20 07:25:51'),(112,'App\\Models\\User','nss655b259740528','auth_token','04341e9e5d5373fa130cd1ce86a05d54546b0bfe0e9fbb0fb15fe533b44d4804','[\"*\"]',NULL,NULL,'2023-11-20 09:35:00','2023-11-20 09:35:00'),(113,'App\\Models\\User','nss655b259740528','auth_token','d2558a2b4ab259b91d6420def8a81ba677f503551497fdf1db9708e46a632daa','[\"*\"]',NULL,NULL,'2023-11-20 09:37:20','2023-11-20 09:37:20'),(114,'App\\Models\\User','nss655b45e8001ec','auth_token','e19b050052057b5d0e83fc4dec3a017c83c9f1270c82d99518c4451574feaeef','[\"*\"]',NULL,NULL,'2023-11-20 09:41:45','2023-11-20 09:41:45'),(115,'App\\Models\\User','nss655b45e8001ec','auth_token','bc614dbd178b0c6c045657c534b77809e9ea2ba11a4be4d9bbd29662783d1e9b','[\"*\"]',NULL,NULL,'2023-11-20 09:42:55','2023-11-20 09:42:55'),(116,'App\\Models\\User','nss655b46988884c','auth_token','680c2d4d065b55cfb0e98e7db5835445ee0b5261c74fa2ccb79e12a41db3a04e','[\"*\"]',NULL,NULL,'2023-11-20 09:44:41','2023-11-20 09:44:41'),(117,'App\\Models\\User','nss655b259740528','auth_token','5271b9d1e87262a1c50c46019bebec279ef1848469dbc4c86090320945a35559','[\"*\"]',NULL,NULL,'2023-11-20 09:46:02','2023-11-20 09:46:02'),(118,'App\\Models\\User','nss655b259740528','auth_token','632eca8d7c733985ed895056d3a84270b3bd0a728e40fb878b7cf321885e6fd1','[\"*\"]',NULL,NULL,'2023-11-20 11:24:19','2023-11-20 11:24:19'),(119,'App\\Models\\User','nss655b259740528','auth_token','257b94730721b7ff1f17ca7ef4effae0330974b1455a33693bfa3a75e6e5e6ce','[\"*\"]',NULL,NULL,'2023-11-20 11:26:21','2023-11-20 11:26:21'),(120,'App\\Models\\User','nss655b45e8001ec','auth_token','8996cdbf93a8472f1026f36784d310918790c0b81d99e7bf1f4bdc177c09ea02','[\"*\"]',NULL,NULL,'2023-11-20 11:31:25','2023-11-20 11:31:25');
+/*!40000 ALTER TABLE `personal_access_tokens` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -95,6 +128,34 @@ CREATE TABLE `posts` (
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `publication_access`
+--
+
+DROP TABLE IF EXISTS `publication_access`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `publication_access` (
+  `access_id` int NOT NULL AUTO_INCREMENT,
+  `account_id` varchar(255) DEFAULT NULL,
+  `post_id` int DEFAULT NULL,
+  PRIMARY KEY (`access_id`),
+  KEY `account_id` (`account_id`),
+  KEY `post_id` (`post_id`),
+  CONSTRAINT `publication_access_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `users` (`account_id`),
+  CONSTRAINT `publication_access_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publication_access`
+--
+
+LOCK TABLES `publication_access` WRITE;
+/*!40000 ALTER TABLE `publication_access` DISABLE KEYS */;
+/*!40000 ALTER TABLE `publication_access` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -134,12 +195,12 @@ DROP TABLE IF EXISTS `roles`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
   `role_id` int NOT NULL AUTO_INCREMENT,
-  `account_id` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `account_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `role` enum('subscriber','admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'subscriber',
   PRIMARY KEY (`role_id`),
   UNIQUE KEY `account_id` (`account_id`),
   CONSTRAINT `fk_account_id` FOREIGN KEY (`account_id`) REFERENCES `users` (`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,6 +209,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (19,'nss655b259740528','subscriber'),(20,'nss655b45e8001ec','admin'),(21,'nss655b46988884c','subscriber');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -228,7 +290,7 @@ CREATE TABLE `user_details` (
   CONSTRAINT `fk_user_details_account` FOREIGN KEY (`account_id`) REFERENCES `users` (`account_id`) ON DELETE CASCADE,
   CONSTRAINT `user_details_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `users` (`account_id`),
   CONSTRAINT `user_details_ibfk_2` FOREIGN KEY (`district_code`) REFERENCES `districts` (`district_code`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -237,7 +299,7 @@ CREATE TABLE `user_details` (
 
 LOCK TABLES `user_details` WRITE;
 /*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
-INSERT INTO `user_details` VALUES (3,'nss6556643379a5d','MZ','ZoloZolo, Botanic','Malawi','0996406730'),(4,'nss65566496d89c4','LL','Area 25, Botanic','Malawi','0996406730'),(5,'nss65566726a33ad','MA','Area 25, Bionic','Malawi','0996406730'),(6,'nss655667b9309dc','MA','Area 25, Bionic','Malawi','0996406730'),(7,'nss65568d937a2e5','BL','Area 25, Bionic','Malawi','0996406730');
+INSERT INTO `user_details` VALUES (34,'nss655b259740528','ZO','Old Naisi, Zomba, Malawi.','Malawi','+265882751146'),(35,'nss655b45e8001ec','BA','Mtandile','Malawi','0996406729'),(36,'nss655b46988884c','NB','Old Naisi, Zomba, Malawi.','Malawi','+265882751146');
 /*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -258,10 +320,16 @@ CREATE TABLE `users` (
   `sex` enum('male','female') NOT NULL,
   `password` varchar(255) NOT NULL,
   `account_id` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`account_id`),
   UNIQUE KEY `user_id` (`user_id`),
+  KEY `users_email_index` (`email`),
+  KEY `users_remember_token_index` (`remember_token`),
   CONSTRAINT `chk_sex` CHECK ((`sex` in (_utf8mb4'male',_utf8mb4'female')))
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -270,7 +338,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (2,'FirstName1','LastName1','username1','email1@example.com','Company1','female','7c6a180b36896a0a8c02787eeafb0e4c','account1'),(4,'Angella','Kanene','Angelaa','angela@jolie.com','Company3','female','819b0643d6b89dc9b579fdfc9094f28e','account3'),(5,'FirstName4','LastName4','username4','email4@example.com','Company4','male','34cc93ece0ba9e3f6f235d4af979b16c','account4'),(8,'Ackim','Longwe','ackimjnr','acklongwe@gmail.com','cloud','male','1234','hash1d'),(9,'Angelina','Jolie','Angel','email@jolie.com','ABC Company','male','secretpassword','nss65563dd19e7fb'),(10,'Azana','Manda','aza','acklongwe@npl.com','ABC Company','female','$2y$12$w8cckW4HQDkugDH9RuH92uEju2FeVXc3RhN1aVs6p/rLWwv2wuLIi','nss65563ed534ec1'),(12,'Azana','Manda','azareali','acklonge@nspl.com','ABC Company','female','$2y$12$31SRQ75foThPWzeAU0CdxOj1vgMpSV2oWDdZl/kzjg2OC3N4D1zQS','nss6556643379a5d'),(13,'boogy','boogy','boogy','boogy@nspl.com','ABC Company','male','$2y$12$ZT7ZgRas97p9et5Lgj8yCuDQE5xSdUuhvokBHi/L0FI1j2.1gb3l.','nss65566496d89c4'),(14,'booogy','booogy','booogy','booogy@nspl.com','ABC Company','male','$2y$12$4y3xPzzmnrKGSY49w21JM.axzd7LCOdEhYOs0029a0LgXEPgFqmPm','nss65566726a33ad'),(15,'booogy','booogy','boooogy','boooogy@nspl.com','ABC Company','male','$2y$12$FfNBwoT/W8MyginM7tjxCuzAs2xCUHYakG1a31.z2X6HqNLo02VEK','nss655667b9309dc'),(16,'booogy','booogy','polon','polony@nspl.com','ABC Company','male','$2y$12$kjbbW6jIJpNZZKrl.TNMKuSnZlwXVYUBh8f667znB.nP2/nuRFahe','nss65568d937a2e5');
+INSERT INTO `users` VALUES (43,'Ackim','Longwe','Ackim','user@gmail.com','National Publications Limited','male','$2y$12$3oMn1FmKkVVJIfVNXW7Q0e4EBDyQulFcUnq4xdZ0bOcLRRUx2wpuq','nss655b259740528',NULL,NULL,'2023-11-20 07:23:35','2023-11-20 07:23:35'),(44,'Jones','Chikwezga','jones','admin@gmail.com','The fossil dream','male','$2y$12$CxOBZZzu.J73FSOvWYcHOOgo1e8s/F1PLJKcO1ygbt.IhbfGyudDm','nss655b45e8001ec',NULL,NULL,'2023-11-20 09:41:28','2023-11-20 09:41:28'),(45,'Ackim','Longwe','jana','jana@gmail.com','jana','male','$2y$12$HMek0dM.ucNlCw4p7il9seOtsCnfxrnQ9m8ZZgV6Z2nkQ0k956bMy','nss655b46988884c',NULL,NULL,'2023-11-20 09:44:24','2023-11-20 09:44:24');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -283,4 +351,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-17 14:26:47
+-- Dump completed on 2023-11-20 15:42:47
